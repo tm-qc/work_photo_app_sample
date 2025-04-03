24eabcc27808157406d4f4d2fe5aeb24337e9e45
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-04-03 13:02:36 +0900

    デザイン個別設定の例2 Theme()ウィジェットによる個別のデザイン設定例
    ThemeDataで全体の色、フォント、ボタン、入力フォームなどを決めるが、個別にデザインを反映する方法の例
    
    Theme()ウィジェットは、そのchildプロパティに指定されたウィジェットツリーに対して、新しいテーマ設定を適用
    
    Theme() を使うのは「複数ボタンに共通で一括で適用したいとき」らしいが長くなるので後で例を記載します。
    
    「1箇所だけ色を変えたい」なら style: ElevatedButton.styleFrom() で直接指定の方が簡潔

--- lib/top_menu.dart
+++ lib/top_menu.dart
@@ -15,8 +15,6 @@ class TopMenu extends StatelessWidget {
             // Theme()ウィジェットによる個別のデザイン設定
             // Theme()ウィジェットは、そのchildプロパティに指定されたウィジェットツリーに対して、新しいテーマ設定を適用
             //
-            // Theme() を使うのは「複数ボタンに共通で一括で適用したいとき」らしいが長くなるので後で例を記載します。
-            //
             // 「1箇所だけ色を変えたい」なら style: ElevatedButton.styleFrom() で直接指定の方が簡潔
             ElevatedButton(
               // style: ElevatedButton.styleFrom()の例
@@ -26,9 +24,34 @@ class TopMenu extends StatelessWidget {
               onPressed: () {}, // 未実装
               child: Text('事業・現場情報ダウンロード'),
             ),
-            ElevatedButton(
-              onPressed: () {}, // 未実装
-              child: Text('写真撮影'),
+            Theme(
+              // Theme()ウィジェットによる個別のデザイン設定例1
+              //
+              // ウィジェット内でテーマ情報を Theme.of(context) で取得できる
+              // copyWithで設定した色で上書き
+              //
+              // 注意
+              // Theme.of() を使うときは context の位置に注意。
+              // build()内ですぐに Theme.of() を読むと取れない場合がある
+              // その場合は、1個下に Builder() を入れる
+              data: Theme.of(context).copyWith(
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green, // ← 緑色に変更
+                  ),
+                ),
+              ),
+              child: ElevatedButton(
+                onPressed: () {}, // 未実装
+                child: Text('写真撮影'),
+              ),
+              // なお一括設定できるとあるが、単純なchild追加では複数設定できないらしい
+              // わかりづらいが一括設定の方法は別途記載する。
+              // ↓これはエラーNG
+              // child: ElevatedButton(
+              //   onPressed: () {}, // 未実装
+              //   child: Text('写真撮影2'),
+              // ),
             ),
             ElevatedButton(
               onPressed: () {
