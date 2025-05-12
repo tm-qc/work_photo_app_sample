d31ed7fb87b986524dd9d1c52457efd4e7d48ec9
tm-qc <taka.hi.rock.music.lovers@gmail.com>
2025-05-12 17:33:09 +0900

    【メモ】TODO:ずっとわからない minSdk = flutter.minSdkVersion の確認方法について
    全体残作業
    ・一部改修の余地ありはTODOで残してる
    
    ※関係性について
    
    Screen(Widget)
    MainのUI画面
    基本的にViewModel経由で情報を取得する
    ただし、Model の定数や固定メニューは直接参照OK
    
    ViewModel
    State定義し、Serviceを参照し情報を取得しScreenに渡す
    
    Service
    Modelからデータを参照し処理し、ViewModelに渡す
    
    Model
    このWidget（Screen）で使うデータを定義する
    プルダウンメニューの内容も定義するのでScreenから直接参照される時もある

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,6 +26,20 @@ android {
         applicationId = "com.work_photo_app_sample.work_photo_app_sample"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
+// TODO:ずっとわからない minSdk = flutter.minSdkVersion の確認方法について
+//
+// Shared preferencesならAndorid SDK 16 以上
+// cameraならならAndorid SDK 21 以上
+//
+//  現状以下の仕様らしい
+//  minSdk = flutter.minSdkVersion のままだと、Flutter内部で定義された**デフォルト値「16」**が使われる
+//
+//  現状今のバージョン、本当に既定が16なのかのシンプルな確認方法がない
+//  過去に「Android SDK version 35.0.1」OKと結論だったが、結局既定が16以上だからのこじつけの話だし、明確に確認する方法がない
+//
+//  一旦cameraが本当に動かないのか確認してみる
+//  動かないならminSdk = flutter.minSdkVersion→minSdk = 21に変更する
+//  影響がわからないから怖いが
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
